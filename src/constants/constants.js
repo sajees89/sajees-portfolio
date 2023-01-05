d08b8b8d5409590ff52e2bff29629591b44c6fd8
Sajees XPS <sajees.jeya@gmail.com>
2023-01-05 02:30:52 -0500

    updated

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: 'MERN',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
+    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application",
       image: '/images/1.png',
       tags: ['Mongo', 'Express', 'React', 'Node'],
     source: 'https://google.com',
@@ -10,7 +10,7 @@ export const projects = [
   },
   {
     title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
+    description:"While building it you're going to learn many advanced React & JavaScript topics",
     image: '/images/2.png',
     tags: ['Mongo', 'Express', 'React', 'Node'],
     source: 'https://google.com',
@@ -38,7 +38,6 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2018, text: 'Enrolled in Udemy course', },
   { year: 2021, text: 'Started my journey', },
   { year: 2022, text: 'Graduated in my Bootcamp', },
   { year: 2023, text: 'Looking for remote work & even relocatation'}
